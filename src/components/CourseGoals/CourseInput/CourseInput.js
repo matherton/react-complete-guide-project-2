0636786469dc5f1bb07f3386324632b144e642ed
Mark Atherton <markatherton@talkholiday.com>
2021-07-27 15:59:11 +0100

    end of lesson 75: changed dynamic inline style to use classes

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -4,15 +4,20 @@ import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
 const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+  const [enteredValue, setEnteredValue] = useState('')
+  const [isValid, setIsValid] = useState(true)
 
   const goalInputChangeHandler = event => {
+    if (enteredValue.trim().length > 0) {
+      setIsValid(true)
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
     if (enteredValue.trim().length === 0) {
+      setIsValid(false)
       return;
     }
     props.onAddGoal(enteredValue);
@@ -20,7 +25,7 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
